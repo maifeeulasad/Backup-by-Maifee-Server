f7b465b4bd07827afabc594b45358759e3b138de
maifeeulasad <maifeeulasad@gmail.com>
2021-04-23 23:09:03 +0600

    added empty spring boot project + extracted

---
+++ src/main/java/com/mua/backupbymaifee/BackupByMaifeeApplication.java
@@ -0,0 +1,13 @@
+package com.mua.backupbymaifee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupByMaifeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupByMaifeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mua/backupbymaifee/BackupByMaifeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mua.backupbymaifee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupByMaifeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
