8f981f12d1d65fea827c561edb7386008cc00f40
maifeeulasad <maifeeulasad@gmail.com>
2021-04-24 22:40:49 +0600

    //refactor

--- src/main/java/com/mua/backupbymaifee/controller/FileController.java
+++ src/main/java/com/mua/backupbymaifee/controller/FileController.java
@@ -1,7 +1,6 @@
 package com.mua.backupbymaifee.controller;
 
 import com.mua.backupbymaifee.service.FileService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +9,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileController {
 
-    private final FileService fileService;
+    private final FileService service;
 
-    public FileController(FileService fileService) {
-        this.fileService = fileService;
+    public FileController(FileService service) {
+        this.service = service;
     }
 
     @PostMapping("/file/upload/")
     Boolean upload(
             @RequestPart("absolute_path") String absolutePath,
-            @RequestPart MultipartFile file){
-        return fileService.upload(absolutePath, file);
+            @RequestPart MultipartFile file) {
+        return service.upload(absolutePath, file);
     }
 
 }
